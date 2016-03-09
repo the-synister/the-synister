7afb7bde54dadf1c058e9c5aab360f61b5999a83
juanecabellob <juanecabellob@gmail.com>
2016-03-09 12:08:24 +0100

    Added files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+var isMac = navigator.platform.toUpperCase().indexOf('MAC')>=0;
+var isWindows = navigator.platform.toUpperCase().indexOf('WIN')>=0;
+
+if (isMac) {
+    $('#download-button').attr('href', 'https://github.com/the-synister/builds/archive/Mac.zip');
+} else if (isWindows) {
+    $('#download-button').attr('href', 'https://github.com/the-synister/builds/archive/Windows.zip');
+} else {
+    $('#mac-windows-support').addClass('hidden');
+    $('#linux-support').removeClass('hidden');
+}
+console.log(isMac);
